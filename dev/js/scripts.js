ca89cce354302d86814e3bb17933c77f5da0e5b6
Ion Prodan <admyn3d@gmail.com>
2017-04-25 20:44:07 +0100

    fixed HTML/CSS for mobile devices

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,122 +1,132 @@
-/* jslint es5: true, multistr: true */
+/* globals $ */
 
-$(document).ready(function() {
-	// https://eonasdan.github.io/bootstrap-datetimepicker/
-	$('.datetimepicker').datetimepicker({
-		format: 'MMM Do, YYYY'
-	});
+$(document).ready(() => {
+  // https://eonasdan.github.io/bootstrap-datetimepicker/
+  $('.datetimepicker').datetimepicker({
+    format: 'MMM Do, YYYY'
+  })
 
-	// https://silviomoreto.github.io/bootstrap-select/
-	$('.selectpicker').selectpicker();
+  // https://silviomoreto.github.io/bootstrap-select/
+  $('.selectpicker').selectpicker()
 
+  // form validation http://jqueryvalidation.org/
+  var form = $('#form-order')
+  form.validate({
+    rules: {
+      'form-client-name': {
+        required: true
+      },
 
-	// form validation http://jqueryvalidation.org/
-	var form = $('#form-order');
-	form.validate({
-		rules: {
-			'form-client-name': {
-				required: true
-			},
+      'form-phone': {
+        required: true
+      },
 
-			'form-phone': {
-				required: true
-			},
+      'form-email': {
+        email: true
+      },
 
-			'form-email': {
-				email: true
-			},
+      'form-moving-from-address': {
+        required: true
+      },
 
-			'form-moving-from-address': {
-				required: true
-			},
+      'form-moving-from-city': {
+        required: true
+      },
 
-			'form-moving-from-city': {
-				required: true
-			},
+      'form-moving-from-zip': {
+        required: true
+      },
 
-			'form-moving-from-zip': {
-				required: true
-			},
+      'form-moving-to-address': {
+      },
 
-			'form-moving-to-address': {
-			},
+      'form-moving-to-city': {
+      },
 
-			'form-moving-to-city': {
-			},
+      'form-moving-to-zip': {
+      },
 
-			'form-moving-to-zip': {
-			},
+      'form-home-type': {
+        required: true
+      },
 
-			'form-home-type': {
-				required: true
-			},
+      'form-form-moving-date': {
+      }
+    }
+  })
 
-			'form-form-moving-date': {
-			}
-		}
-	});
+  form.validate()
 
-	form.validate();
-
-
-	// remove placeholder on focus
-	$('input[type="text"]')
-		.focus( function(){
-			$(this).attr("data-placeholder", $(this).attr('placeholder')).removeAttr("placeholder");
-		})
-		// add placeholder on focus out if input is empty
-		.blur( function(){
-			$(this).attr("placeholder", $(this).attr('data-placeholder'));
-		});
-});
-
+  // remove placeholder on focus
+  $('input[type="text"]')
+    .focus(() => {
+      $(this).attr('data-placeholder', $(this).attr('placeholder')).removeAttr('placeholder')
+    })
+    // add placeholder on focus out if input is empty
+    .blur(() => {
+      $(this).attr('placeholder', $(this).attr('data-placeholder'))
+    })
+})
 
 // send data from form to email
-var sendFormData = function() {
-	var clientName = $('#form-client-name').val();
-	var clientPhone = $('#form-phone').val();
-	var clientEmail = $('#form-email').val();
-	var fromAddress = $('#form-moving-from-address').val();
-	var fromCity = $('#form-moving-from-city').val();
-	var fromZip = $('#form-moving-from-zip').val();
-	var toAddress = $('#form-moving-to-address').val();
-	var toCity = $('#form-moving-to-city').val();
-	var toZip = $('#form-moving-to-zip').val();
-	var homeType = $('#form-home-type').val();
-	var movingDate = $('#form-moving-date').val();
+const sendFormData = () => {
+  const clientName = $('#form-client-name').val()
+  const clientPhone = $('#form-phone').val()
+  const clientEmail = $('#form-email').val()
+  const fromAddress = $('#form-moving-from-address').val()
+  const fromCity = $('#form-moving-from-city').val()
+  const fromZip = $('#form-moving-from-zip').val()
+  const toAddress = $('#form-moving-to-address').val()
+  const toCity = $('#form-moving-to-city').val()
+  const toZip = $('#form-moving-to-zip').val()
+  const homeType = $('#form-home-type').val()
+  const movingDate = $('#form-moving-date').val()
 
-	var message = '\
-		Name: ' + clientName + '\n' +'\
-		Phone: ' + clientPhone + '\n' +'\
-		Email: ' + clientEmail + '\n' +'\
-		Pick up address: ' + fromAddress + '\n' +'\
-		From city: ' + fromCity + '\n' +'\
-		From zip: ' + fromZip + '\n' +'\
-		Destionation address: ' + toAddress + '\n' +'\
-		To city: ' + toCity + '\n' +'\
-		To zip: ' + toZip + '\n' +'\
-		Home type: ' + homeType + '\n' +'\
-		Moving date: ' + movingDate + '\n';
+  // var message = '\
+  //   Name: ' + clientName + '\n' +'\
+  //   Phone: ' + clientPhone + '\n' +'\
+  //   Email: ' + clientEmail + '\n' +'\
+  //   Pick up address: ' + fromAddress + '\n' +'\
+  //   From city: ' + fromCity + '\n' +'\
+  //   From zip: ' + fromZip + '\n' +'\
+  //   Destionation address: ' + toAddress + '\n' +'\
+  //   To city: ' + toCity + '\n' +'\
+  //   To zip: ' + toZip + '\n' +'\
+  //   Home type: ' + homeType + '\n' +'\
+  //   Moving date: ' + movingDate + '\n';
 
-	$.ajax({
-		url: 'contact.php',
-		type: 'POST',
-		data: { 'message': message }
-	});
+  const message = `
+    Name: ${clientName}
+    Phone: ${clientPhone}
+    Email: ${clientEmail}
+    Pick up address: ${fromAddress}
+    From city: ${fromCity}
+    From zip: ${fromZip}
+    Destionation address: ${toAddress}
+    To city: ${toCity}
+    To zip: ${toZip}
+    Home type: ${homeType}
+    Moving date: ${movingDate}`
 
-	if ($('#form-order').valid())
-		$('#form-modal').modal('toggle');
-};
+  $.ajax({
+    url: 'contact.php',
+    type: 'POST',
+    data: { 'message': message }
+  })
 
+  if ($('#form-order').valid()) {
+    $('#form-modal').modal('toggle')
+  }
+}
 
 // add .active class to clicked link
-$(".nav a").on("click", function(){
-	$(".nav").find(".active").removeClass("active");
-	$(this).parent().addClass("active");
+$('.nav a').on('click', () => {
+  $('.nav').find('.active').removeClass('active')
+  $(this).parent().addClass('active')
 
-	if ($('#navbar-collapse').attr('aria-expanded')  == 'true')
-		$('#navbar-collapse').collapse('toggle');
-});
+  if ($('#navbar-collapse').attr('aria-expanded') === 'true') {
+    $('#navbar-collapse').collapse('toggle')
+  }
+})
 
-
-var clock = $('.your-clock').FlipClock();
+const clock = $('.your-clock').FlipClock()
--- gulpfile.js
+++ gulpfile.js
@@ -1,112 +1,121 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var uglifyCss = require('gulp-uglifycss');
-var jshint = require('gulp-jshint');
-var myth = require('gulp-myth');
-var plumber = require('gulp-plumber');
-var rename = require('gulp-rename');
-var jade = require('gulp-jade');
-var beeper = require('beeper');
-var browserSync = require('browser-sync');
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const uglifyJs = require('gulp-uglify')
+const uglifyCss = require('gulp-uglifycss')
+const sass = require('gulp-sass')
+// const jshint = require('gulp-jshint')
+const myth = require('gulp-myth')
+const plumber = require('gulp-plumber')
+const rename = require('gulp-rename')
+const jade = require('gulp-jade')
+const beeper = require('beeper')
+const browserSync = require('browser-sync')
+const babel = require('gulp-babel')
 
-var onError = function(err) {
-	beeper();
-	console.log(err);
-};
-
+const onError = err => {
+  beeper()
+  console.log(err)
+}
 
 var cssFiles = [
-	'dev/css/bootstrap.css',
-	'dev/css/bootstrap-datetimepicker.css',
-	'dev/css/bootstrap-select.css',
-	'dev/css/font-awesome.css',
-	'dev/css/*.css'
-];
+  'dev/css/bootstrap.css',
+  'dev/css/bootstrap-datetimepicker.css',
+  'dev/css/bootstrap-select.css',
+  'dev/css/font-awesome.css',
+  'dev/css/*.css'
+]
 
-gulp.task('styles', function() {
-	gulp.src(cssFiles)
-		.pipe(plumber({
-		  errorHandler: onError
-		}))
-		.pipe(concat('css/style.css'))
-		.pipe(myth())
-		.pipe(gulp.dest('build'));
-});
+// SASS taks
+gulp.task('sass', () => {
+  gulp
+    .src('dev/css/*.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('dev/css'))
+})
 
+gulp.task('styles', () => {
+  gulp
+    .src(cssFiles)
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(concat('css/style.css'))
+    .pipe(myth())
+    .pipe(gulp.dest('build'))
+})
 
-gulp.task('minifyCss', function() {
-	gulp.src('build/css/style.css')
-		.pipe(uglifyCss())
-		.pipe(rename('css/style.min.css'))
-		.pipe(gulp.dest('build'));
-});
+gulp.task('minifyCss', () => {
+  gulp
+    .src('build/css/style.css')
+    .pipe(uglifyCss())
+    .pipe(rename('css/style.min.css'))
+    .pipe(gulp.dest('build'))
+})
 
+gulp.task('jade', () => {
+  gulp
+    .src('dev/*.jade')
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(jade())
+    .pipe(gulp.dest('build/'))
+})
 
-gulp.task('jade', function() {
-	gulp.src('dev/*.jade')
-		.pipe(plumber({
-			errorHandler: onError
-		}))
-		.pipe(jade())
-		.pipe(gulp.dest('build/'))
-});
-
-
-gulp.task('js', function() {
-	gulp.src('dev/js/*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'))
-		.pipe(concat('js/scripts.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('build'))
-});
-
+gulp.task('js', () => {
+  gulp
+    .src('dev/js/*.js')
+    // .pipe(jshint())
+    // .pipe(jshint.reporter('default'))
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(concat('js/scripts.js'))
+    .pipe(uglifyJs())
+    .pipe(gulp.dest('build'))
+})
 
 var libsFiles = [
-	'dev/libs/jquery-1.11.3.js',
-	'dev/libs/moment.js',
-	'dev/libs/jquery.validate.js',
-	'dev/libs/bootstrap.js',
-	'dev/libs/bootstrap-datetimepicker.js',
-	'dev/libs/bootstrap-select.js',
-	'dev/libs/*.js'
-];
+  'dev/libs/jquery-1.11.3.js',
+  'dev/libs/moment.js',
+  'dev/libs/jquery.validate.js',
+  'dev/libs/bootstrap.js',
+  'dev/libs/bootstrap-datetimepicker.js',
+  'dev/libs/bootstrap-select.js',
+  'dev/libs/*.js'
+]
 
-gulp.task('libs', function() {
-	gulp.src(libsFiles)
-		// .pipe(jshint())
-		// .pipe(jshint.reporter('default'))
-		.pipe(concat('libs/libs.js'))
-		.pipe(gulp.dest('build'))
-});
+gulp.task('libs', () => {
+  gulp
+    .src(libsFiles)
+    // .pipe(jshint())
+    // .pipe(jshint.reporter('default'))
+    .pipe(concat('libs/libs.js'))
+    .pipe(gulp.dest('build'))
+})
 
-
-gulp.task('minifyLibs', function() {
-  gulp.src('build/libs/libs.js')
-    .pipe(uglify())
+gulp.task('minifyLibs', () => {
+  gulp
+    .src('build/libs/libs.js')
+    .pipe(uglifyJs())
     .pipe(rename('libs/libs.min.js'))
-    .pipe(gulp.dest('build'));
-});
+    .pipe(gulp.dest('build'))
+})
 
+gulp.task('browserSync', cb => {
+  browserSync({
+    server: {
+      baseDir: './build'
+    }
+  }, cb)
+})
 
-gulp.task('browserSync', function(cb) {
-	browserSync({
-		server: {
-			baseDir: './build'
-		}
-	}, cb);
-});
+gulp.task('watch', () => {
+  gulp.watch('dev/css/*.sass', ['sass'])
+  gulp.watch('dev/css/*.css', ['styles'])
+  gulp.watch('build/css/style.css', ['minifyCss', browserSync.reload])
+  gulp.watch('dev/*.jade', ['jade', browserSync.reload])
+  gulp.watch('dev/js/*.js', ['js', browserSync.reload])
+  gulp.watch('dev/libs/*.js', ['libs', browserSync.reload])
+  gulp.watch('build/libs/libs.js', ['minifyLibs', browserSync.reload])
+})
 
-
-gulp.task('watch', function() {
-	gulp.watch('dev/css/*.css', ['styles']);
-	gulp.watch('build/css/style.css', ['minifyCss', browserSync.reload]);
-	gulp.watch('dev/*.jade', ['jade', browserSync.reload]);
-	gulp.watch('dev/js/*.js', ['js']);
-	gulp.watch('dev/libs/*.js', ['libs']);
-	gulp.watch('build/libs/libs.js', ['minifyLibs']);
-});
-
-
-gulp.task('default', ['styles', 'minifyCss', 'jade', 'libs', 'js', 'minifyLibs', 'browserSync', 'watch']);
+gulp.task('default', ['sass', 'styles', 'minifyCss', 'jade', 'libs', 'js', 'minifyLibs', 'browserSync', 'watch'])
